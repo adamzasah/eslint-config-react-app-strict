d67c8ae2ccfa69cf264c928bef1877e3851f8997
Adam Folwarczny <adam@adamfolwarczny.com>
2018-11-08 16:49:10 +0100

    Add source

---
+++ index.js
@@ -0,0 +1,80 @@
+module.exports = {
+    "extends": "react-app",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "legacyDecorators": true
+        }
+    },
+    "rules": {
+        "semi": [
+            "warn",
+            "always"
+        ],
+        "indent": [
+            "warn",
+            4,
+            {
+                "FunctionDeclaration": {
+                    "parameters": "off"
+                },
+                "FunctionExpression": {
+                    "parameters": "off"
+                },
+                "MemberExpression": "off",
+                "SwitchCase": 1
+            }
+        ],
+        "comma-spacing": [
+            "warn",
+            {
+                "before": false,
+                "after": true
+            }
+        ],
+        "quotes": [
+            "warn",
+            "single",
+            {
+                "allowTemplateLiterals": true
+            }
+        ],
+        "brace-style": [
+            "warn",
+            "1tbs",
+            {
+                "allowSingleLine": false
+            }
+        ],
+        "object-curly-spacing": [
+            "warn",
+            "never"
+        ],
+        "array-bracket-spacing": [
+            "warn",
+            "never"
+        ],
+        "radix": "off",
+        "no-useless-escape": "off",
+        "default-case": "off",
+        "import/first": "off",
+        "no-extend-native": "off",
+        "curly": "warn",
+        "react/jsx-tag-spacing": [
+            "warn",
+            {
+                "beforeSelfClosing": "never"
+            }
+        ],
+        "react/jsx-curly-brace-presence": [
+            "warn",
+            {
+                "props": "always",
+                "children": "ignore"
+            }
+        ],
+        "react/jsx-boolean-value": [
+            "warn",
+            "always"
+        ]
+    }
+};
